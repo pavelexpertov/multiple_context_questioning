c44403e53b81d6d44f3be3307f84ecde15631209
pavelexpertov <pavelm01@hotmail.co.uk>
2024-08-20 22:46:09 +0100

    Make front end receive requests

--- front_end_service/src/main.rs
+++ front_end_service/src/main.rs
@@ -1,10 +1,20 @@
 #[macro_use] extern crate rocket;
 
-use reqwest;
+use core::future::Future;
 
-#[get("/")]
-async fn index() -> &'static str {
-    "Hello, I am working!"
+use rocket::form::Form;
+use rocket::State;
+use reqwest::{self, Error, Response, Client};
+
+#[derive(FromForm)]
+struct UserRequest {
+    question: String
+}
+
+#[post("/prompt", data="<user_input>")]
+async fn prompt(user_input: Form<UserRequest>) -> String {
+    let response = send_single_prompt(&user_input.question).await.unwrap().text().await.unwrap();
+    response
 }
 
 #[get("/single_request")]
@@ -21,5 +31,23 @@ async fn single_request() -> String {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, single_request])
+    rocket::build().mount("/", routes![prompt, single_request])
 }
+
+fn send_single_prompt(prompt: &str) -> impl Future<Output = Result<Response, Error>> {
+    let params = [("question", prompt)];
+    let client = Client::new();
+	client.post("http://localhost:8000/single_prompt")
+        .form(&params)
+        .send()
+}
+
+
+//GET /single_prompt HTTP/1.1
+//Accept: *[>
+//Accept-Encoding: gzip, deflate
+//Connection: keep-alive
+//Content-Length: 28
+//Content-Type: application/x-www-form-urlencoded; charset=utf-8
+//Host: localhost:8000
+//User-Agent: HTTPie/2.6.0
